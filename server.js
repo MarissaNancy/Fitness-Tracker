3a32c1e504a418ecef73ca2f6cf73634e7164b0f
Nancy Marissa Gonzalez <nancygonzalez7@yahoo.com>
2021-07-09 19:36:24 -0700

    added a route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const logger = require("morgan");//
+const logger = require("morgan");
+const path = require("path");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3000;
@@ -14,16 +15,22 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethoddb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true  });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}!`);
-  });
+app.get("/", (req, res) => {
+  res.send(index.html);
+});
 
-//statshtml api js is linked line40
+app.get("/exercise", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/exercise.html"));
+});
 
-app.get("/workout", (req, res) => {
+app.get("/stats", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/stats.html"));
+});
+
+
+app.get("/exercise", (req, res) => {
   db.Workout.find({})
   .then(dbWorkout => {
       res.json(dbWorkout);
@@ -33,44 +40,52 @@ app.get("/workout", (req, res) => {
   });
 });
 
-app.put
+app.put("/exercise/:id", function(req, res) {
+    if (error) {
+      console.log(error);
+    }
+    else {
+    const newExercise = req.body;
+    const exerciseLists = found[0].exercises;
+    exerciseLists.push(newExercise);
+    db.Workout.updateOne(
+      { 
+        _id: mongojs.ObjectId(req.params.id)
+      },
+      {
+        $set : { exercises: exerciseLists }
+      },
+      (error, edited) => {
+        if (error){
+          console.log(error);
+        } else {
+          res.send(edited);
+        }
+      }
+    )
+  }
+});
 
-app.post("/workout", (req, res) => {
+
+app.post("/exercise", (req, res) => {
   db.Workout.create({})
   .then(data => {
       res.json(data);
   });
 });
 
+app.get("/workouts/range", (req, res) =>{
+  db.Workout.find({})
+  .then(dbWorkout => {
+    while(dbWorkout.length > 7){
+      let wout = db.Workout.shift()
+    }
+    res.json(dbWorkout)
+  })
+})
 
 
-app.get("/", (req, res) => {
-  res.send(index.html);
-});
-
-app.post("/exercise", ({ body }, res) => {
-  db.Workout.create(body)
-    .then((newWorkout) => {
-      res.json(newWorkout);
-    })
-    .catch((err) => {
-      res.json(err);
-    });
-});
-//create new workout > WORK
-// app.post("/excercise", async ({ body }, res) => {
-//     try {
-//        const workoutDT = await Workout.create(body)
-//         res.status(200).json(workoutDT);
-// }catch(err) {
-//         res.status(400).json(err);
-//     }
-// });
-// app.get("/excercise?", async ({ body }, res) => {
-//     try {
-//         const prevWorkout = await Workout.findOneAndUpdate({})
-//     }
-// })
+// Start the server
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
